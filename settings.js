2294ecfeb4cbbdeee572456792e5b00f1e69ca5b
Lostsoulyt <clintondan321@gmail.com>
2024-10-09 19:39:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,8 +11,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916378704507"
-global.ownername = "AASHISH"
+global.ownernomer = "2349131804593"
+global.ownername = "Broken"
 global.ytname = "YT: SHADOW STARCORE"
 global.socialm = "GitHub: dark-tech-zone"
 global.location = "India, Delhi, city"
